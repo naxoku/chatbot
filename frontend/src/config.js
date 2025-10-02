9fa578bd9374c2af73dcd4b93b83c8305c426a91
sebacatalan <scatalan2020@alu.uct.cl>
2025-10-02 15:46:23 -0300

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const API_BASE = "chatbot-production-d56e.up.railway.app";
+export const API_BASE = "https://chatbot-production-d56e.up.railway.app";
 //export const API_BASE = "http://localhost:3000";
 export const LOGOUT = `${API_BASE}/auth/logout`;
 export const CHECK_SESSION = `${API_BASE}/auth/checkSession`;
